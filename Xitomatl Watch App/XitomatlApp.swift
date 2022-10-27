e8a74f6b00c25e77487adf0dd6525ac7bdb06e3a
Alvaro Mattus-Donaire <varodonaire@me.com>
2022-10-26 22:02:45 -0300

    Initial Commit

---
+++ Xitomatl Watch App/XitomatlApp.swift
@@ -0,0 +1,17 @@
+//
+//  XitomatlApp.swift
+//  Xitomatl Watch App
+//
+//  Created by Alvaro on 26-10-22.
+//
+
+import SwiftUI
+
+@main
+struct Xitomatl_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
